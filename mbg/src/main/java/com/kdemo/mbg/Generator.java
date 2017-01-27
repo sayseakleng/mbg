e14b4c8914be4d23f6dd2fc3c1fcc12bd04ee6ff
Mr.SAY SEAK LENG <Mr.SAY SEAK LENG@192.168.1.106>
2017-01-27 23:33:49 +0700

    Initial commit

---
+++ mbg/src/main/java/com/kdemo/mbg/Generator.java
@@ -0,0 +1,33 @@
+package com.kdemo.mbg;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.api.VerboseProgressCallback;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class Generator {
+	public static void main(String[] args) throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		
+//		File configFile = new File("generatorConfig.xml");
+		InputStream resourceAsStream = Generator.class.getResourceAsStream("/generatorConfig.xml");
+		
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(resourceAsStream);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		
+		
+		myBatisGenerator.generate(new VerboseProgressCallback());
+	}
+}
